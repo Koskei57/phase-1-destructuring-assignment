6be420d93c5cf8371fc90a10e52e15f829d30ff8
Koskei57 <dennis.koskei@student.moringaschool.com>
2022-06-12 21:02:24 +0300

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,54 @@
-const farmAnimals = 'cow horse sheep pig chicken';
+//strings
+const farmAnimals = "cow horse sheep pig chicken";
 
+
+//Arrays
+//uses destructuring to declare the seven traditional rainbow color variables using the color names
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+console.log(red, orange, yellow, green, blue, indigo, violet);
+//uses destructuring to declare six rainbow color variables using initials
+const [r, o, y, g, b, i, v] = colors;
+console.log(red, orange, green, blue, violet);
+//uses destructuring to declare Indigo using indg
+const [, , , , , indg] = colors;
+console.log(indigo);
 
+//objects
 const muppet = {
-  muppetName: 'Miss Piggy',
-  color: 'pink',
-  song: 'Never Before, Never Again',
-  job: 'Cast member of The Muppet Show',
-  partner: 'Kermit'
+    muppetName: 'Miss Piggy',
+    color: 'pink',
+    song: 'Never Before, Never Again',
+    job: 'Cast member of The Muppet Show',
+    partner: 'Kermit'
 };
 
 const nestedMuppet = {
-  nestedName: 'Kermit',
-  nestedColor: 'green',
-  album: {
-    theMuppetMovie: {
-      song1: 'Rainbow Connection',
-      song2: 'Moving Right Along',
-      song3: 'Never Before, Never Again',
-      song4: 'I Hope That Something Better Comes Along',
+    nestedName: 'Kermit',
+    nestedColor: 'green',
+    album: {
+        theMuppetMovie: {
+            song1: 'Rainbow Connection',
+            song2: 'Moving Right Along',
+            song3: 'Never Before, Never Again',
+            song4: 'I Hope That Something Better Comes Along',
+        },
     },
-  },
-  nestedJob: 'Host of The Muppet Show',
-  nestedPartner: 'Miss Piggy'
+    nestedJob: 'Host of The Muppet Show',
+    nestedPartner: 'Miss Piggy'
 };
+const { muppetName, color, song, job, partner } = muppet;
+const { nestedJob, nestedPartner, album: { theMuppetMovie: { song2, song4 } } } = nestedMuppet;
+
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split('');
+console.log(moo);
+//uses destructuring to declear the four traditional animal names. 
+
+const [bessie, , dolly, babe, little] = farmAnimals.split('');
+console.log(bessie, dolly, babe, little);
+//uses destructuring to declare the three traditional animal colors
+const [blackAndWhite, , black, pink] = farmAnimals.split('');
+console.log(blackAndWhite, black, pink);
 
 // Strings
 
